6033ca6d1592f685608cf1cfe769bef74afd8ad5
Mackleroy <gatt2508@gmail.com>
2021-09-15 18:22:07 +0300

    Delete models witch were used before redis

--- sites/admin.py
+++ sites/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-
-from sites.models import Visit
-
-
-@admin.register(Visit)
-class VisitAdmin(admin.ModelAdmin):
-    list_display = ['domain', 'datetime']
-    search_fields = ['domain', 'datetime']
--- sites/models.py
+++ sites/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-
-class Visit(models.Model):
-    domain = models.CharField(max_length=100)
-    datetime = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.domain
